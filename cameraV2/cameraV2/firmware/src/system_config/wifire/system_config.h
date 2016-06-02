84d1ca84e4ccd00da18b9232020f8c62c7e50ae7
MattAtHazmat <matt.bennett@gmail.com>
2016-06-01 20:55:44 -0500

    It's sending I2C, just not getting a NACK.
    	modified:   camera/firmware/camera.X/nbproject/configurations.xml
    	new file:   cameraV2/cameraV2/firmware/cameraV2.X/Makefile
    	new file:   cameraV2/cameraV2/firmware/cameraV2.X/nbproject/configurations.xml
    	new file:   cameraV2/cameraV2/firmware/cameraV2.X/nbproject/project.xml
    	new file:   cameraV2/cameraV2/firmware/cameraV2.hconfig
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_AGC.c
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_AGC.h
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_ErrorCodes.h
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_I2C_Protocol.c
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_I2C_Protocol.h
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_I2C_Reg.h
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_I2C_Service.c
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_I2C_Service.h
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_Macros.h
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_SDK.c
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_SDK.h
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_SDKConfig.h
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_SYS.c
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_SYS.h
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_Types.h
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_VID.c
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/LEPTON_VID.h
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/Makefile
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/aardvark.c
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/aardvark.h
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/aardvark_I2C.c
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/aardvark_I2C.h
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/crc16.h
    	new file:   cameraV2/cameraV2/firmware/src/LeptonSDKEmb32PUB/crc16fast.c
    	new file:   cameraV2/cameraV2/firmware/src/comms.c
    	new file:   cameraV2/cameraV2/firmware/src/comms.h
    	new file:   cameraV2/cameraV2/firmware/src/flir.c
    	new file:   cameraV2/cameraV2/firmware/src/flir.h
    	new file:   cameraV2/cameraV2/firmware/src/main.c
    	new file:   cameraV2/cameraV2/firmware/src/system_config/wifire/FreeRTOSConfig.h
    	new file:   cameraV2/cameraV2/firmware/src/system_config/wifire/configuration.xml
    	new file:   cameraV2/cameraV2/firmware/src/system_config/wifire/framework/system/clk/src/sys_clk_static.c
    	new file:   cameraV2/cameraV2/firmware/src/system_config/wifire/framework/system/clk/sys_clk_static.h
    	new file:   cameraV2/cameraV2/firmware/src/system_config/wifire/framework/system/ports/src/sys_ports_static.c
    	new file:   cameraV2/cameraV2/firmware/src/system_config/wifire/rtos_hooks.c
    	new file:   cameraV2/cameraV2/firmware/src/system_config/wifire/system_config.h
    	new file:   cameraV2/cameraV2/firmware/src/system_config/wifire/system_definitions.h
    	new file:   cameraV2/cameraV2/firmware/src/system_config/wifire/system_exceptions.c
    	new file:   cameraV2/cameraV2/firmware/src/system_config/wifire/system_init.c
    	new file:   cameraV2/cameraV2/firmware/src/system_config/wifire/system_interrupt.c
    	new file:   cameraV2/cameraV2/firmware/src/system_config/wifire/system_interrupt_a.S
    	new file:   cameraV2/cameraV2/firmware/src/system_config/wifire/system_tasks.c
    	new file:   cameraV2/cameraV2/firmware/src/system_config/wifire/wifire.mhc

---
+++ cameraV2/cameraV2/firmware/src/system_config/wifire/system_config.h
@@ -0,0 +1,256 @@
+/*******************************************************************************
+  MPLAB Harmony System Configuration Header
+
+  File Name:
+    system_config.h
+
+  Summary:
+    Build-time configuration header for the system defined by this MPLAB Harmony
+    project.
+
+  Description:
+    An MPLAB Project may have multiple configurations.  This file defines the
+    build-time options for a single configuration.
+
+  Remarks:
+    This configuration header must not define any prototypes or data
+    definitions (or include any files that do).  It only provides macro
+    definitions for build-time configuration options that are not instantiated
+    until used by another MPLAB Harmony module or application.
+    
+    Created with MPLAB Harmony Version 1.07
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2013-2015 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED AS IS WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+#ifndef _SYSTEM_CONFIG_H
+#define _SYSTEM_CONFIG_H
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+/*  This section Includes other configuration headers necessary to completely
+    define this configuration.
+*/
+#include "bsp_config.h"
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+// DOM-IGNORE-END 
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Service Configuration
+// *****************************************************************************
+// *****************************************************************************
+// *****************************************************************************
+/* Common System Service Configuration Options
+*/
+#define SYS_VERSION_STR           "1.07"
+#define SYS_VERSION               10700
+
+// *****************************************************************************
+/* Clock System Service Configuration Options
+*/
+#define SYS_CLK_FREQ                        200000000ul
+#define SYS_CLK_BUS_PERIPHERAL_1            100000000ul
+#define SYS_CLK_BUS_PERIPHERAL_2            100000000ul
+#define SYS_CLK_BUS_PERIPHERAL_3            100000000ul
+#define SYS_CLK_BUS_PERIPHERAL_4            100000000ul
+#define SYS_CLK_BUS_PERIPHERAL_5            100000000ul
+#define SYS_CLK_BUS_PERIPHERAL_7            200000000ul
+#define SYS_CLK_BUS_PERIPHERAL_8            100000000ul
+#define SYS_CLK_CONFIG_PRIMARY_XTAL         24000000ul
+#define SYS_CLK_CONFIG_SECONDARY_XTAL       0ul
+   
+/*** Interrupt System Service Configuration ***/
+#define SYS_INT                     true
+
+/*** Ports System Service Configuration ***/
+
+#define SYS_PORT_A_ANSEL        0x603
+#define SYS_PORT_A_TRIS         0xc6ff
+#define SYS_PORT_A_LAT          0x0
+#define SYS_PORT_A_ODC          0x0
+#define SYS_PORT_A_CNPU         0x0
+#define SYS_PORT_A_CNPD         0x0
+#define SYS_PORT_A_CNEN         0x0
+
+#define SYS_PORT_B_ANSEL        0xf7ff
+#define SYS_PORT_B_TRIS         0xf7ff
+#define SYS_PORT_B_LAT          0x0
+#define SYS_PORT_B_ODC          0x0
+#define SYS_PORT_B_CNPU         0x0
+#define SYS_PORT_B_CNPD         0x0
+#define SYS_PORT_B_CNEN         0x0
+
+#define SYS_PORT_C_ANSEL        0xe01e
+#define SYS_PORT_C_TRIS         0xf01e
+#define SYS_PORT_C_LAT          0x0
+#define SYS_PORT_C_ODC          0x0
+#define SYS_PORT_C_CNPU         0x0
+#define SYS_PORT_C_CNPD         0x0
+#define SYS_PORT_C_CNEN         0x0
+
+#define SYS_PORT_D_ANSEL        0xc000
+#define SYS_PORT_D_TRIS         0xfe2f
+#define SYS_PORT_D_LAT          0x0
+#define SYS_PORT_D_ODC          0x0
+#define SYS_PORT_D_CNPU         0x0
+#define SYS_PORT_D_CNPD         0x0
+#define SYS_PORT_D_CNEN         0x0
+
+#define SYS_PORT_G_ANSEL        0x200
+#define SYS_PORT_G_TRIS         0x7383
+#define SYS_PORT_G_LAT          0x0
+#define SYS_PORT_G_ODC          0x0
+#define SYS_PORT_G_CNPU         0x0
+#define SYS_PORT_G_CNPD         0x0
+#define SYS_PORT_G_CNEN         0x0
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Configuration
+// *****************************************************************************
+// *****************************************************************************
+/*** Timer Driver Configuration ***/
+#define DRV_TMR_INTERRUPT_MODE             true
+#define DRV_TMR_INSTANCES_NUMBER           2
+#define DRV_TMR_CLIENTS_NUMBER             1
+
+/*** Timer Driver 0 Configuration ***/
+#define DRV_TMR_PERIPHERAL_ID_IDX0          TMR_ID_2
+#define DRV_TMR_INTERRUPT_SOURCE_IDX0       INT_SOURCE_TIMER_2
+#define DRV_TMR_INTERRUPT_VECTOR_IDX0       INT_VECTOR_T2
+#define DRV_TMR_ISR_VECTOR_IDX0             _TIMER_2_VECTOR
+#define DRV_TMR_INTERRUPT_PRIORITY_IDX0     INT_PRIORITY_LEVEL1
+#define DRV_TMR_INTERRUPT_SUB_PRIORITY_IDX0 INT_SUBPRIORITY_LEVEL0
+#define DRV_TMR_CLOCK_SOURCE_IDX0           DRV_TMR_CLKSOURCE_INTERNAL
+#define DRV_TMR_PRESCALE_IDX0               TMR_PRESCALE_VALUE_256
+#define DRV_TMR_OPERATION_MODE_IDX0         DRV_TMR_OPERATION_MODE_16_BIT
+#define DRV_TMR_ASYNC_WRITE_ENABLE_IDX0     false
+#define DRV_TMR_POWER_STATE_IDX0            SYS_MODULE_POWER_RUN_FULL
+
+/*** Timer Driver 1 Configuration ***/
+#define DRV_TMR_PERIPHERAL_ID_IDX1          TMR_ID_3
+#define DRV_TMR_INTERRUPT_SOURCE_IDX1       INT_SOURCE_TIMER_3
+#define DRV_TMR_INTERRUPT_VECTOR_IDX1       INT_VECTOR_T3
+#define DRV_TMR_ISR_VECTOR_IDX1             _TIMER_3_VECTOR
+#define DRV_TMR_INTERRUPT_PRIORITY_IDX1     INT_PRIORITY_LEVEL1
+#define DRV_TMR_INTERRUPT_SUB_PRIORITY_IDX1 INT_SUBPRIORITY_LEVEL0
+#define DRV_TMR_CLOCK_SOURCE_IDX1           DRV_TMR_CLKSOURCE_INTERNAL
+#define DRV_TMR_PRESCALE_IDX1               TMR_PRESCALE_VALUE_256
+#define DRV_TMR_OPERATION_MODE_IDX1         DRV_TMR_OPERATION_MODE_16_BIT
+#define DRV_TMR_ASYNC_WRITE_ENABLE_IDX1     false
+#define DRV_TMR_POWER_STATE_IDX1            SYS_MODULE_POWER_RUN_FULL
+
+ // *****************************************************************************
+/* I2C Driver Configuration Options
+*/
+#define DRV_I2C_INTERRUPT_MODE                    		true
+#define DRV_I2C_CLIENTS_NUMBER                    		1
+#define DRV_I2C_INSTANCES_NUMBER                  		1
+
+#define DRV_I2C_PERIPHERAL_ID_IDX0                		I2C_ID_4
+#define DRV_I2C_OPERATION_MODE_IDX0               		DRV_I2C_MODE_MASTER
+#define DRV_SCL_PORT_IDX0                               PORT_CHANNEL_G
+#define DRV_SCL_PIN_POSITION_IDX0                       PORTS_BIT_POS_8
+#define DRV_SDA_PORT_IDX0                               PORT_CHANNEL_G
+#define DRV_SDA_PIN_POSITION_IDX0                       PORTS_BIT_POS_7
+#define DRV_I2C_BIT_BANG_IDX0                           false
+#define DRV_I2C_STOP_IN_IDLE_IDX0                       false
+#define DRV_I2C_SMBus_SPECIFICATION_IDX0			    false
+#define DRV_I2C_BAUD_RATE_IDX0                    		50000
+#define DRV_I2C_BRG_CLOCK_IDX0	                  		100000000
+#define DRV_I2C_SLEW_RATE_CONTROL_IDX0      			false
+#define DRV_I2C_MASTER_INT_SRC_IDX0               		INT_SOURCE_I2C_4_MASTER
+#define DRV_I2C_SLAVE_INT_SRC_IDX0                		
+#define DRV_I2C_ERR_MZ_INT_SRC_IDX0               		INT_SOURCE_I2C_4_BUS
+#define DRV_I2C_MASTER_INT_VECTOR_IDX0            		INT_VECTOR_I2C4_MASTER
+#define DRV_I2C_MASTER_ISR_VECTOR_IDX0                  _I2C4_MASTER_VECTOR
+#define DRV_I2C_MASTER_INT_PRIORITY_IDX0          		INT_PRIORITY_LEVEL3
+#define DRV_I2C_MASTER_INT_SUB_PRIORITY_IDX0      		INT_SUBPRIORITY_LEVEL3
+#define DRV_I2C_SLAVE_INT_VECTOR_IDX0             		INT_VECTOR_I2C4_SLAVE
+#define DRV_I2C_SLAVE_ISR_VECTOR_IDX0			  	    _I2C4_SLAVE_VECTOR
+#define DRV_I2C_SLAVE_INT_PRIORITY_IDX0           		
+#define DRV_I2C_SLAVE_INT_SUB_PRIORITY_IDX0       		
+#define DRV_I2C_ERR_INT_VECTOR_IDX0               		INT_VECTOR_I2C4_BUS
+#define DRV_I2C_ERR_ISR_VECTOR_IDX0                     _I2C4_BUS_VECTOR
+#define DRV_I2C_ERR_INT_PRIORITY_IDX0             		INT_PRIORITY_LEVEL3
+#define DRV_I2C_ERR_INT_SUB_PRIORITY_IDX0         		INT_SUBPRIORITY_LEVEL0
+#define DRV_I2C_POWER_STATE_IDX0                  		SYS_MODULE_POWER_RUN_FULL
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Middleware & Other Library Configuration
+// *****************************************************************************
+// *****************************************************************************
+/*** OSAL Configuration ***/
+#define OSAL_USE_RTOS          1
+
+// *****************************************************************************
+/* BSP Configuration Options
+*/
+#define BSP_OSC_FREQUENCY 24000000
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Configuration
+// *****************************************************************************
+// *****************************************************************************
+
+/*** Application Instance 0 Configuration ***/
+
+#define FLIR_LED            BSP_LED_3
+#define FLIR_TMR_DRV                       1
+#define FLIR_TMR_DRV_IS_PERIODIC           true
+#define FLIR_TMR_DRV_PERIOD                0xfe51
+
+/*** Application Instance 1 Configuration ***/
+
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
+}
+#endif
+//DOM-IGNORE-END
+
+
+#endif // _SYSTEM_CONFIG_H
+/*******************************************************************************
+ End of File
+*/
+
